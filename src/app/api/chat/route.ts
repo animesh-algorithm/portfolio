511078b0af70e4f997d089c852d8e0c7e97a4d0a
Animesh Sharma <animesharma3@gmail.com>
2024-02-04 18:55:17 +0530

    Fixing Serverless function timeout issue

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -17,6 +17,8 @@ import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 import { createHistoryAwareRetriever } from "langchain/chains/history_aware_retriever";
 import { createRetrievalChain } from "langchain/chains/retrieval";
 
+export const maxDuration = 30;
+
 export async function POST(req: Request) {
   try {
     const body = await req.json();
